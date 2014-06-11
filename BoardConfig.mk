3fe053db1fe733a6374f67b10bf052f50f4ed0b5
Vishal Dubey <yzvishal.vd@gmail.com>
2014-06-11 01:14:04 -0400

    update device tree

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,13 +15,15 @@
 -include device/semc/mogami-common/BoardConfigCommon.mk
 -include vendor/semc/iyokan/BoardConfigVendor.mk
 
-TARGET_SPECIFIC_HEADER_PATH += device/semc/iyokan/include
+TARGET_SPECIFIC_HEADER_PATH += device/semc/iyokan/include \
+
+$(shell mkdir -p out/target/product/iyokan/obj/KERNEL_OBJ/usr)
 
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/semc/iyokan/bluedroid
 
 # Kernel
-TARGET_KERNEL_CONFIG := lx_iyokan_defconfig
+TARGET_KERNEL_CONFIG := nAa_iyokan_defconfig
 
 # Sensors
 SOMC_CFG_SENSORS_ACCEL_BMA150_INPUT := yes
